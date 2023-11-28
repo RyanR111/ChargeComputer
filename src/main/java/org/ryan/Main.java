8177bd5af55d4c16e73a83ce339a560111a1610f
RyanR111 <6234930@edu.vaniercollege.qc.ca>
2023-11-27 23:23:34 -0500

    Finishing Charge method and Battery object. One problem must be fixed in the Charge method

--- src/main/java/org/ryan/Main.java
+++ src/main/java/org/ryan/Main.java
@@ -1,7 +1,10 @@
 package org.ryan;
 
+import org.ryan.dto.Battery;
+
 public class Main {
     public static void main(String[] args) {
-
+        Battery mainBat = new Battery();
+        mainBat.Charge();
     }
 }
--- src/main/java/org/ryan/dto/Battery.java
+++ src/main/java/org/ryan/dto/Battery.java
@@ -1,15 +1,48 @@
 package org.ryan.dto;
 
-public class Battery {
-    private boolean charge;
-    private int batteryPercent;
+import java.util.Scanner;
 
-    public boolean Charge() {
-        while (true) {
+public class Battery {
+    Scanner console = new Scanner(System.in);
+    private boolean charge;
+    public int batteryPercent;
+    public boolean plugged;
+
+
+    public void Charge() {
+        System.out.println("Plugged? ");
+        plugged = console.nextBoolean();
+        if (plugged) {
+            System.out.println("Enter charge value: ");
+            batteryPercent = console.nextInt();
+        }
+
+        while (plugged) {
             if (batteryPercent < 40) {
                 charge = true;
+                while (batteryPercent <= 60) {
+                    System.out.println(charge);
+                    System.out.println("Plugged? ");
+                    plugged = console.nextBoolean();
+                    System.out.println("Enter charge value: ");
+                    batteryPercent = console.nextInt();
+                    if (!plugged) {
+                        break;
+                    }
+                }
+            } else if (batteryPercent >= 60) {
+                charge = false;
+                while (batteryPercent >= 40) {
+                    System.out.println(charge);
+                    System.out.println("Plugged? ");
+                    plugged = console.nextBoolean();
+                    System.out.println("Enter charge value: ");
+                    batteryPercent = console.nextInt();
+                    if (!plugged) {
+                        break;
+                    }
+                }
             }
-
         }
     }
 }
